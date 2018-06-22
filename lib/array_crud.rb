604e2c0c7a6feb09d6c2711facfa13dff008a404
Jordan Harris <jharris@wework.com>
2018-06-22 16:38:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,32 @@
+require 'pry'
+
 def create_an_empty_array
-  
+ new_array = Array.new
+ return new_array
 end
 
 def create_an_array
-  
+  my_array = Array.new
+  my_array.push("1", "2", "3", "4")
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = Array.new
+  my_array = my_array.push(array)
+  my_array = my_array.push(element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = Array.new
+  my_array = my_array.push(array)
+  my_array = my_array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  my_array = Array.new
+  my_array = my_array.push(array)
+binding.pry
+  my_array = my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
